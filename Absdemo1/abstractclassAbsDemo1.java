2e5fd3c65079a98a588f1b14bde767945921255b
Anushathalla <thallaanusha5312@gmail.com>
2024-02-23 09:23:41 +0530

    absdemo

---
+++ Absdemo1/abstractclassAbsDemo1.java
@@ -0,0 +1,27 @@
+package Absdemo1;
+
+public abstract class abstractclassAbsDemo1
+{
+	void AbsDemo1()
+	{
+		System.out.println("cons of AbsDemo1");
+	}
+	public static void main(String[] args) 
+	{
+//		AbsDemo1 ab1 = new AbsDemo1();
+//		ab1. func();//abstract is key cannot create object is there implemented  and unimplemented function avaible
+	}
+	abstract void funA();
+	abstract void funB();
+	
+	void funC()
+	{
+		System.out.println("funC of AbsDemo1");
+	
+	}
+}
+		
+	
+	
+
+
